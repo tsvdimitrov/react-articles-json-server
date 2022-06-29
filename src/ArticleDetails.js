4516e725b6a51377d7354c993e54939aaa671d86
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-06-29 15:14:41 +0300

    Create ArticleDetails.js

---
+++ src/ArticleDetails.js
@@ -0,0 +1,21 @@
+import { useParams } from 'react-router-dom';
+
+const ArticleDetails = () => {
+    const { id } = useParams();
+    const { data: article, error, isPending } = useFetch('http://localhost:8000/articles/' + id);
+
+    return (
+        <div className="article-details">
+            {isPending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            <article >
+                <img>{article.image}</img>
+                <h2>{article.title}</h2>
+                <h3>{article.excerpt}</h3>
+                <h3>{article.category}</h3>
+            </article>
+        </div>
+    );
+}
+
+export default ArticleDetails;
