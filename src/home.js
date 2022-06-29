2a13304f128ca5a06d32522f330c568e8a827bdd
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-06-29 13:15:11 +0300

    Create home.js and prepare to fetch from localhost with error and loading messages

---
+++ src/home.js
@@ -0,0 +1,14 @@
+
+const Home = () => {
+    const { data: articles, isPending, error } = useFetch('http://localhost:8000/articles');
+
+    return (
+        <div className="home">
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading...</div>}
+            <ArticleList articles={articles} title="All Articles" />
+        </div>
+    );
+}
+
+export default Home;
